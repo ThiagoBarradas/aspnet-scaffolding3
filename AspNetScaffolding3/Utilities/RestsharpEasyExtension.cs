b1263cc531d09bddebde7b8b72c4e60ba6e8aa2e
Thiago Barradas <th.barradas@gmail.com>
2020-03-10 18:32:32 -0300

    :art: add asactionresult for baseresponse

---
+++ AspNetScaffolding3/Utilities/RestsharpEasyExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp.Easy.Models;
+using WebApi.Models.Response;
+
+namespace AspNetScaffolding.Utilities
+{
+    public static class RestsharpEasyExtension
+    {
+        public static IActionResult AsActionResult<TResponse>(this BaseResponse<TResponse, ErrorsResponse> response)
+        {
+            object responseObj = response.Data;
+
+            if (responseObj == null || !response.Is2XX)
+            {
+                responseObj = response.Error;
+            }
+
+            return new ObjectResult(responseObj)
+            {
+                StatusCode = (int) response.StatusCode
+            };
+        }
+    }
+}
