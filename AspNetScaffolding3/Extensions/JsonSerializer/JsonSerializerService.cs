44d9df8acb01f06d8c000e4204f3b79c55a0b878
Thiago Barradas <th.barradas@gmail.com>
2020-04-27 20:23:11 -0300

    :arrow_up: update dependencies

--- AspNetScaffolding3/Extensions/JsonSerializer/JsonSerializerService.cs
+++ AspNetScaffolding3/Extensions/JsonSerializer/JsonSerializerService.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using PackUtils;
 using PackUtils.Converters;
 using System;
